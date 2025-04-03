422900806cb7582a89276075823926dcded4c939
ErnesTo <eernestoj21@gmail.com>
2025-04-03 17:47:55 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022SC653-2022VM650/Models/PedidoDetalle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L02P02_2022SC653_2022VM650.Models;
+
+public partial class PedidoDetalle
+{
+    public int Id { get; set; }
+
+    public int? IdPedido { get; set; }
+
+    public int? IdLibro { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Libro? IdLibroNavigation { get; set; }
+
+    public virtual PedidoEncabezado? IdPedidoNavigation { get; set; }
+}

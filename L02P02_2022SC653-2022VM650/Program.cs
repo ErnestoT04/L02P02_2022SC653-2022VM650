e196b1dc08392e45fb1aa1c30c49e43a02847aa1
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-04-03 19:08:11 -0600

    commit

--- L02P02_2022SC653-2022VM650/Program.cs
+++ L02P02_2022SC653-2022VM650/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Prototipo01}/{action=Index}/{id?}");
 
 app.Run();
